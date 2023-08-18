5b8f9e3526256188d2aa573b21ed8a1c5b41f485
07ty <rstoyo.yuri.tsushima@gmail.com>
2023-08-18 17:27:45 +0900

    修正2

--- test/controllers/prototypes_controller_test.rb
+++ test/controllers/prototypes_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class PrototypesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get prototypes_index_url
+    get prototypes_path
     assert_response :success
   end
 end
