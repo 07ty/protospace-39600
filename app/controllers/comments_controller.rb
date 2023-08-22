bcf3d61ae97264bf896f6e18f2e22f1a31516210
07ty <rstoyo.yuri.tsushima@gmail.com>
2023-08-22 11:24:31 +0900

    update controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,17 @@
 class CommentsController < ApplicationController
   def create
-    @prototype = Prototype.find(params[:prototype_id])
-    @comment = @prototype.comments.build(comment_params)
-    @comment.user = current_user
-
+    @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to prototype_path(@prototype), notice: 'コメントが投稿されました。'
+      redirect_to prototype_path(@comment.prototype) 
     else
-      redirect_to prototype_path(@prototype), alert: 'コメントの投稿に失敗しました。'
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
+      render "prototypes/show"
     end
   end
 
   private
-
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
 end
