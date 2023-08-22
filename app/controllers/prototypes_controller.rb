c6750cc8d0f426ff148be860ee5fe24900fab87d
07ty <rstoyo.yuri.tsushima@gmail.com>
2023-08-22 11:17:53 +0900

    update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,23 +1,42 @@
 class PrototypesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show, :new]
-  def show
-    @prototype = Prototype.find(params[:id])
-    @comment = Comment.new
-    @comments = @prototype.comments.includes(:user)
-  end
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @prototypes = Prototype.includes(:user)
   end
 
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+  
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    @prototype.update(prototype_params)
+    if @prototype.save
+      redirect_to prototype_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def new
+    #プロトタイプオブジェクトを生成
     @prototype = Prototype.new
   end
-    
+
   def create
     @prototype = current_user.prototypes.new(prototype_params)
     if @prototype.save
-      redirect_to prototype_path(@prototype), notice: 'プロトタイプが作成されました。'
+      redirect_to prototypes_path
     else
       render :new, status: :unprocessable_entity
     end
@@ -28,31 +47,17 @@ class PrototypesController < ApplicationController
     @prototype.destroy
     redirect_to root_path
   end
-    
-  def edit
-    @prototype = Prototype.find(params[:id])
-  end
-      
-  def update
-    @prototype = Prototype.find(params[:id])
-    if @prototype.update(prototype_params)
-      redirect_to root_path
-    else
-      puts @prototype.errors.full_messages # バリデーションエラーメッセージを表示
-      render :edit, status: :unprocessable_entity
-    end
-  end
 
   private
-    
+
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image)
   end
-    
-    def move_to_index
+
+  def move_to_index
     access_prototype = Prototype.find(params[:id])
     unless user_signed_in? && access_prototype.user == current_user
-    redirect_to action: :index
+      redirect_to action: :index
     end
   end
 end
